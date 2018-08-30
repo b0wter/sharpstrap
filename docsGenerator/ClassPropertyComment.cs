e90873e6cf928bc1e02e3779ff50a3c0e90996fb
Joe <b0wter@gmx.de>
2018-08-30 13:16:24 +0200

    Added class to store property information and fixed retrieval of classes form code files.

---
+++ docsGenerator/ClassPropertyComment.cs
@@ -0,0 +1,10 @@
+namespace DocsGenerator
+{
+    internal struct ClassPropertyComment
+    {
+        public string ClassName { get; set; }
+        public string PropertyName { get; set; }
+        public string Comment { get; set; }
+        public string Type { get; set; }
+    }
+}
--- docsGenerator/CodeAnalysis.cs
+++ docsGenerator/CodeAnalysis.cs
@@ -7,64 +7,36 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace DocsGenerator
 {
-    public class CodeFile
+    internal class ClassCodeInfo
     {
-        private SyntaxNode root;
+        private IEnumerable<ClassDeclarationSyntax> classSyntaxes;
 
-        private CodeFile() { }
+        private ClassCodeInfo() { }
 
-        public static CodeFile FromSourceCodeFile(string filename)
+        internal static ClassCodeInfo FromSourceCodeFile(string filename)
         {
             if(System.IO.File.Exists(filename) == false)
                 throw new System.IO.FileNotFoundException($"The file '{filename}' could not be found.");
 
             var fileContent = System.IO.File.ReadAllText(filename);
             var tree = CSharpSyntaxTree.ParseText(fileContent);
-            
-            var code = new CodeFile();
-            code.root = tree.GetRoot();
+            var compilationRoot = tree.GetCompilationUnitRoot();
+
+            var classes = compilationRoot
+                            .Members
+                            .OfType<NamespaceDeclarationSyntax>()
+                            .SelectMany(
+                                x => x.Members.OfType<ClassDeclarationSyntax>()
+                            );
+
+            var code = new ClassCodeInfo();
+            code.classSyntaxes = classes;
             return code;
         }
 
-        public void GetPropertiesWithComments()
+        internal IEnumerable<ClassPropertyComment> GetPropertiesWithComments()
         {
-            var root = (CompilationUnitSyntax) this.root;
-
-            var classDeclarations = root.Members.Where(x => x.GetType().IsAssignableFrom(typeof(ClassDeclarationSyntax)));
-
-            FindAllClassesInNode(root);
-            foreach(var element in classDeclarations)
-            {
-                Console.WriteLine($"{element.GetText()}");
-            }
-            //var classNode = (ClassDeclarationSyntax) root.Members.First();
-
-        }
-
-        //private IList<ClassDeclarationSyntax> FindAllClassesInNode(SyntaxNode node)
-        private void FindAllClassesInNode(SyntaxNode node)
-        {
-            foreach(var child in node.ChildNodes())
-            {
-                Console.WriteLine(child.Kind().ToString());
-                FindAllClassesInNode(child);
-            }
-
-            /*
-            var syntax = (CompilationUnitSyntax) node;
-
-            if(syntax.Members.Count == 0)
-            {
-                return new List<ClassDeclarationSyntax>(1);
-            }
-            else
-            {
-                var temp = new List<ClassDeclarationSyntax>();
-                foreach(var element in syntax.Members)
-                    temp.AddRange(FindAllClassesInNode(element));
-                return temp;
-            }
-            */
+            throw new NotImplementedException();
         }
     }
 }

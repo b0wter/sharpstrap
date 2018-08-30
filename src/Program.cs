3f8076aa39466ad730ec0bd64bedf1b59c4ef7c0
b0wter <b0wter@gmx.de>
2018-08-30 16:50:13 +0200

    Added a 'validate' command. Configs are now always validated before they are run.

--- src/Program.cs
+++ src/Program.cs
@@ -21,6 +21,12 @@ namespace SharpStrap
                 return;
             }
 
+            if(args.Length == 2 && args[0] == "validate")
+            {
+                ValidateBootstrapConfig(args[1]);
+                return;
+            }
+
             if((args.Length != 1 && args.Length != 2) || File.Exists(args.Last()) == false)
             {
                 Console.WriteLine("This tool requires at least parameter (config file). You may add the '-y' option before the filename to autorun the bootstrap.");
@@ -28,6 +34,9 @@ namespace SharpStrap
             }
 
             bool overrideUserDecision = args.First() == "-y";
+
+            if(ValidateBootstrapConfig(args.Last()) == false)
+                return;
 
             using(var reader = File.OpenText(args.Last()))
             {
@@ -43,6 +52,63 @@ namespace SharpStrap
                     Console.WriteLine(ex.Message);
                 }
             }
+        }
+
+        private static bool ValidateBootstrapConfig(string filename)
+        {
+            if(System.IO.File.Exists(filename) == false)
+            {
+                Console.WriteLine($"The given file does not exist.");
+                return false;
+            }
+
+            var deserializer = CreateDefaultDeserializer();
+            try
+            {
+                using(var reader = File.OpenText(filename))
+                {
+                    var bootstrap = deserializer.Deserialize<Bootstrap>(reader);
+                    Console.WriteLine("File parsed succesfully.");
+                }
+            }
+            catch(YamlException ex)
+            {
+                Console.WriteLine($"There is at least one error in the file:");
+                Console.WriteLine($"{ex.Message}");
+                if(ex.InnerException != null)
+                    Console.WriteLine(ex.InnerException.Message);
+                Console.WriteLine("Config file:");
+                Console.WriteLine("[ .. ]");
+
+
+                const int marginLines = 3;
+                var startLine = Math.Max(0, ex.Start.Line - marginLines);
+                var numberOfRelevantLines = ex.End.Line - ex.Start.Line + 2 * marginLines + 1;
+                var relevantConfigFileContent = File.ReadAllLines(filename).Skip(startLine).Take(numberOfRelevantLines).ToArray();
+
+                for(int i = 0; i < numberOfRelevantLines; ++i)
+                {
+                    var paddedLineNumber = (i + startLine).ToString().PadLeft(4);
+                    var currentLine = i + startLine;
+                    if(currentLine >= ex.Start.Line && currentLine <= ex.End.Line)
+                    {
+                        Console.Out.Flush();
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"[{paddedLineNumber}] > {relevantConfigFileContent[i]}");
+                    }
+                    else
+                    {
+                        Console.Out.Flush();
+                        Console.ResetColor();
+                        Console.WriteLine($"[{paddedLineNumber}]   {relevantConfigFileContent[i]}");
+                    }
+                }
+
+                Console.WriteLine("[ .. ]");
+                return false;
+            }
+
+            return true;
         }
 
         private static void PrintAllKnownModules()
